cba2b17ad9d3557650af0867e4078e8ef286797a
Jack <jack.blackfire.interactive@gmail.com>
2026-04-02 23:18:40 +0100

    summ

--- docs/resources/script.js
+++ docs/resources/script.js
@@ -1,5 +1,6 @@
 let isLight = false;
 
+// Light mode
 function ToggleLightMode()
 {
     const lightToggleButton = document.getElementById("light-toggle-button");
@@ -16,4 +17,16 @@ function ToggleLightMode()
         isLight = false;
         lightToggleButton.textContent = "Light Mode";
     }
+}
+
+// Tabs for the land activities
+function LandHome()
+{
+    document.getElementById('heading').textContent = "Land";
+    document.getElementById('content').textContent = "Visitors can take part in a wide range of land-based activities on and off site.";
+}
+function LandHillwalking()
+{
+    document.getElementById('heading').textContent = "Hill Walking";
+    document.getElementById('content').textContent = "From short walks around the site to Munroe-bagging expeditions, Lochquarry has it all! Walks can be tailored to suit any age or experience of groups and can last from one hour to all day adventures. Max group size 12. Ages 6+.";
 }
